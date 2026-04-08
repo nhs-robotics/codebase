393848862d82ec8d97beb8a7c8b9810bdb9f1c64
liam <ltb271@students.needham.k12..ma.us>
2026-04-07 20:02:49 -0400

    add MotorTestTeleop

---
+++ TeamCode/src/main/java/codebase/MotorTestTeleop.java
@@ -0,0 +1,83 @@
+package codebase;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import java.util.List;
+import java.util.Locale;
+
+import codebase.gamepad.Gamepad;
+import codebase.hardware.Motor;
+
+@TeleOp(name="Motor Test Teleop")
+public class MotorTestTeleop extends OpMode {
+    private Gamepad gamepad;
+    private List<Motor> motors;
+
+
+    private Telemetry.Item currentMotorIndexTelemetry;
+    private int currentMotorIndex = 0;
+
+    private Telemetry.Item motorModeTelemetry;
+    private boolean velocityMode = false;
+
+    private Telemetry.Item motorPowerOrVelocityTargetTelemetry;
+    private double targetVelocityTicksPerSecond = 0;
+
+    @Override
+    public void init() {
+        for (DcMotorEx motor : hardwareMap.getAll(DcMotorEx.class)) {
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            motors.add(new Motor(motor));
+        }
+
+        if (motors.isEmpty()) {
+            throw new IllegalStateException("No motors found :(");
+        }
+
+        gamepad.xButton.onPress(() -> currentMotorIndex = (currentMotorIndex - 1 + motors.size()) % motors.size());
+
+        gamepad.bButton.onPress(() -> currentMotorIndex = (currentMotorIndex + 1) % motors.size());
+        gamepad.yButton.onPress(() -> velocityMode = !velocityMode);
+
+        gamepad.dpadLeft.onPress(() -> targetVelocityTicksPerSecond -= 50);
+        gamepad.dpadRight.onPress(() -> targetVelocityTicksPerSecond += 50);
+
+
+        telemetry.addLine("--- Motor Test Controls ---");
+        telemetry.addLine("X/B: prev/next motor");
+        telemetry.addLine("Y: toggle Power/Velocity mode");
+        telemetry.addLine("DPad L/R: velocity target -/+50");
+        telemetry.addLine("Right Stick Y: set power (If in Power mode)");
+        telemetry.addLine("---------------------------");
+
+        currentMotorIndexTelemetry = telemetry.addData("Current Index", currentMotorIndex);
+        motorModeTelemetry = telemetry.addData("Mode", velocityMode ? "Velocity" : "Power");
+        motorPowerOrVelocityTargetTelemetry = telemetry.addData("", getCurrentPowerOrVelocityTargetOutput());
+    }
+
+    private String getCurrentPowerOrVelocityTargetOutput() {
+        if (velocityMode) {
+            return String.format(Locale.US, "Target Velocity: %.2f, Current Velocity: %.2f", targetVelocityTicksPerSecond, motors.get(currentMotorIndex).getVelocity());
+        } else {
+            return String.format(Locale.US, "Current Power: %.2f", motors.get(currentMotorIndex).getPower());
+        }
+    }
+
+    @Override
+    public void loop() {
+        gamepad.loop();
+        currentMotorIndexTelemetry.setValue(currentMotorIndex);
+        motorModeTelemetry.setValue("Mode", velocityMode ? "Velocity" : "Power");
+
+        if (velocityMode) {
+            motors.get(currentMotorIndex).setVelocity(targetVelocityTicksPerSecond);
+        } else {
+            motors.get(currentMotorIndex).setPower(gamepad.rightJoystick.getY());
+        }
+    }
+}
